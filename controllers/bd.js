98ba6b6179d6140b7b5155d8a0282588cf6cfc98
antigx <tiago.mamedio.barbosa@gmail.com>
2023-10-07 00:32:59 -0300

    CRUD fully connceted to sqlite db

--- controllers/bd.js
+++ controllers/bd.js
@@ -12,7 +12,8 @@ export const createTable = () => {
     "CREATE TABLE IF NOT EXISTS todo (id VARCHAR(40) PRIMARY KEY, title VARCHAR(100), text VARCHAR(500), color VARCHAR(7), favorite BOOL)"
   );
 };
-export const insertTodo = (id, title, text, color, favorite) => {
+
+export const insertDBToDo = (id, title, text, color, favorite) => {
   const sql = db.prepare(
     "INSERT INTO todo (id, title, text, color, favorite) VALUES (?, ?, ?, ?, ?)"
   );
@@ -28,13 +29,18 @@ export const getDBTodos = (callback) => {
       callback(err, null); // Pass error to the callback
       return;
     }
-    const jsonData = JSON.stringify(rows); // Convert rows to JSON
-
     callback(rows); // Pass JSON data to the callback
   });
 };
 
-export const updateTodo = (id, updatedProperties) => {
+export const deleteDBToDo = (id) => {
+  const sql = db.prepare("DELETE FROM todo WHERE id = ?");
+  sql.run(id);
+  console.log("Sucesses deletion");
+  sql.finalize();
+};
+
+export const updateDBToDo = (id, updatedProperties) => {
   const keys = Object.keys(updatedProperties);
   const values = Object.values(updatedProperties);
   const setStatements = keys.map((key) => `${key} = ?`).join(", ");
